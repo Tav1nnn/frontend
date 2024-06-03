fd93dc7ab7484116c3d0ae3149dd341cce6963a5
Otavio Marques <marquesotavio251204@gmail.com>
2024-06-03 19:43:12 -0300

    aula 02 react

---
+++ aula02/my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+    return (
+      <div>
+        <h1>3 x 0</h1>
+        <img src="https://s3.static.brasilescola.uol.com.br/be/2023/09/1-escudo-do-corinthians.jpg"
+         alt="logo"/>
+      </div>
+    );
+}
+
+export default App;
